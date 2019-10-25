0b7b017f4ce51d2bc7b17c97af3a934b5e9df608
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 13:38:51 +1100

    Update models.py

--- djangogirls/website/blog/models.py
+++ djangogirls/website/blog/models.py
@@ -1,14 +1,12 @@
 # All of the text in here was edited by me. 
 #This file is where I defined my blog posts. 
-#This file was also where I added DO_NOTHING in line 8 to ask terminal to overwrite the error message that was preventing me processing forward
-#models.foreignkey is a link to another model but was not processing as there was not another model to link to
 # blog/models.py
 
 from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.DO_NOTHING)
+    author = models.ForeignKey('auth.User',on_delete=models.DO_NOTHING) #added DO_NOTHING in line 8 to ask terminal to overwrite the error message that was preventing me processing forward
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
