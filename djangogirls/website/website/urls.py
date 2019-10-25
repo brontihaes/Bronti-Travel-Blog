7ddd9364978be6587bc2e7ce87d658abbf15e715
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 11:29:57 +1100

    Update urls.py

--- djangogirls/website/website/urls.py
+++ djangogirls/website/website/urls.py
@@ -1,3 +1,5 @@
+#This file is where all the URL patterns are stored to enable the site to work
+#Line 6 was edited in this file as I added the inclusion of blog.urls so that Django could redirect everything to this command
 from django.conf.urls import include, url
 from django.contrib import admin
 
