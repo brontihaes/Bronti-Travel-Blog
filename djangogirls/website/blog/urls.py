92ec95433a889fadff30e5372f015d242a636f4d
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 14:35:05 +1100

    Update urls.py

--- djangogirls/website/blog/urls.py
+++ djangogirls/website/blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('post/<pk>/remove/', views.post_remove, name='post_remove'), #added to enable a delete button to appear
     path('drafts/', views.post_draft_list, name='post_draft_list'), #added to create a draft page on my blog
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'), #added to create a comments form page
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'), #added to be able to moderate comments
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'), #added to be able to remove comments
 ]
