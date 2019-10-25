5c757501c87c125a17ca4145c99a935687ad0c20
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 11:34:41 +1100

    Update views.py

--- djangogirls/website/blog/views.py
+++ djangogirls/website/blog/views.py
@@ -1,3 +1,5 @@
+#Lines 5 and 8-12 were edited here by me.
+#This file links the views with the HTML templates so posts are viewed within the same structure
 # blog/views.py
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
