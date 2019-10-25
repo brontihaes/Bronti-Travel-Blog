d718729f7b7417b78dd62df2b7e7b5afc2e4ee51
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 13:49:42 +1100

    Update urls.py

--- djangogirls/website/blog/urls.py
+++ djangogirls/website/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'), #edited to extend my application and connect the html extensions I created 
     path('post/<pk>/publish/', views.post_publish, name='post_publish'), #added to enable a publish button to appear
     path('post/<pk>/remove/', views.post_remove, name='post_remove'), #added to enable a delete button to appear
+    path('drafts/', views.post_draft_list, name='post_draft_list'), #added to create a draft page on my blog
 ]
