0a7fdeefb19f183dd78a456918dde499e695d0eb
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 13:35:34 +1100

    Update views.py

--- djangogirls/website/blog/views.py
+++ djangogirls/website/blog/views.py
@@ -1,15 +1,18 @@
-#Lines 5 and 8-12 were edited here by me.
 #This file links the views with the HTML templates so posts are viewed within the same structure
-#Line 7 and 11 were added as my posts were not displaying, once including them my posts appeared
+
 # blog/views.py
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post #enabled models to import posts
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.all() #added this line to enable my posts to appear on the blog.
+    return render(request, 'blog/post_list.html', {'posts': posts}) #added posts: post 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+def post_publish(request, pk): #added these command lines to add a publish button
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
