d33e162678926f9cadc8e871d25eee9cb289d1b1
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 14:00:07 +1100

    Update admin.py

--- djangogirls/website/blog/admin.py
+++ djangogirls/website/blog/admin.py
@@ -2,6 +2,7 @@
 # blog/admin.py
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post) #makes my model visible on the admin page
+admin.site.register(Comment) #makes my comment visible on admin page 
