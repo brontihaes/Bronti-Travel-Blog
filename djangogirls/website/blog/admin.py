bde9a857081be5cf58a284796a188d586ab4e65e
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 13:39:24 +1100

    Update admin.py

--- djangogirls/website/blog/admin.py
+++ djangogirls/website/blog/admin.py
@@ -1,8 +1,7 @@
 #This file adds, edits and deletes posts via the admin url.
-# Line 8 makes my model visible on the admin page
 # blog/admin.py
 
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+admin.site.register(Post) #makes my model visible on the admin page
