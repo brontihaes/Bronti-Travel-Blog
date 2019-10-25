a8bab2f8a1d7dbf36d8825abd169da931c47d63a
brontihaes <56900202+brontihaes@users.noreply.github.com>
2019-10-25 13:37:55 +1100

    Update urls.py

--- djangogirls/website/blog/urls.py
+++ djangogirls/website/blog/urls.py
@@ -1,11 +1,9 @@
-#Line 8 was edited to extend my application and connect the html extensions I created 
-#This command was then linked in blog.views and imported to my URL
-#I added line 10 in to enable a publish button to appear
+#This file is where all the URL's are entered and linked to the HTML
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #edited to extend my application and connect the html extensions I created 
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'), #added to enable a publish button to appear
 ]
